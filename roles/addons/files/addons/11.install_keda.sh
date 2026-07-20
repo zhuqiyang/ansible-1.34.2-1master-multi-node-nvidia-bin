12f0b701fbeeee0eae0879afcef1689a50125063
root <qiqitao320@qq.com>
2026-07-20 17:57:02 +0800

    add file

---
+++ roles/addons/files/addons/11.install_keda.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# helm repo add kedacore https://kedacore.github.io/charts
+# helm pull kedacore/keda
+
+tar -xf keda-2.20.1.tgz
+
+helm install keda . \
+  --namespace keda \
+  --create-namespace \
+  --set resources.operator.requests.cpu=100m \
+  --set resources.operator.requests.memory=128Mi \
+  --set metricsAdapter.podIdentity.activeDirectory.enabled=false 
