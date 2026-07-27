a6a29a402f511b9e570f077a32bc6f06c345731a
root <qiqitao320@qq.com>
2026-07-27 21:06:01 +0800

    comment out nvidia-device-plugin install

--- roles/addons/files/addons/6.install_nvidia-device-plugin.sh
+++ roles/addons/files/addons/6.install_nvidia-device-plugin.sh
@@ -6,30 +6,30 @@
 # helm pull nvdp/nvidia-device-plugin --version 0.18.0
 
 
-cat > runtimeclass.yaml <<EOF
-apiVersion: node.k8s.io/v1
-kind: RuntimeClass
-metadata:
-  name: nvidia
-handler: nvidia
-EOF
+# cat > runtimeclass.yaml <<EOF
+# apiVersion: node.k8s.io/v1
+# kind: RuntimeClass
+# metadata:
+#   name: nvidia
+# handler: nvidia
+# EOF
 
-kubectl apply -f runtimeclass.yaml
+# kubectl apply -f runtimeclass.yaml
 
 
 
-tar -xf nvidia-device-plugin-0.18.0.tgz
+# tar -xf nvidia-device-plugin-0.18.0.tgz
 
-cd nvidia-device-plugin
+# cd nvidia-device-plugin
  
 # 将 runtimeClassName: null 修改为 runtimeClassName: nvidia
-sed -i 's/runtimeClassName: null/runtimeClassName: nvidia/g' values.yaml
+# sed -i 's/runtimeClassName: null/runtimeClassName: nvidia/g' values.yaml
 
 # 将 values.yaml 中的 64到85行注释掉
-sed -i '64,85s/^/#/g' values.yaml
+# sed -i '64,85s/^/#/g' values.yaml
 
 # 安装 nvidia-device-plugin
-helm install nvidia-device-plugin -n kube-system .
+# helm install nvidia-device-plugin -n kube-system .
 
 
 # kubectl label nodes <node-name> feature.node.kubernetes.io/pci-10de.present=true
